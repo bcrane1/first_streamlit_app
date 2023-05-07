5d227f9148cf79a15bcfe7ee048454b9f96ea2bc
bcrane1 <101469842+bcrane1@users.noreply.github.com>
2023-05-06 20:51:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinch & Rocket Smothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacodo Toast')
    
