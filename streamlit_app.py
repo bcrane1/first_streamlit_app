106bccef4bd41b208c535ae0df95175f8e49efa7
bcrane1 <101469842+bcrane1@users.noreply.github.com>
2023-05-08 23:29:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.stop()
 #import snowflake.connector
 #Snowflake related functions
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
     return my_cur.fetchall()
 
